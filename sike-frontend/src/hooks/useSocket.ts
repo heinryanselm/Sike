4a442c5c6266706bfca9ece6cb4b8eb3e8795b6a
Heinry-Anselm <50972784+anselm-chilz@users.noreply.github.com>
2024-11-26 10:22:33 +0000

    Further updates for vercel deployment

--- sike-frontend/src/hooks/useSocket.ts
+++ sike-frontend/src/hooks/useSocket.ts
@@ -24,3 +24,30 @@ export const useSocket = () => {
 
   return socket;
 };
+const useLocalStorage = (key: string, initialValue: unknown) => {
+  const [storedValue, setStoredValue] = useState(() => {
+    if (typeof window === "undefined") {
+      return initialValue;
+    }
+
+    try {
+      const item = window.localStorage.getItem(key);
+      return item ? JSON.parse(item) : initialValue;
+    } catch {
+      return initialValue;
+    }
+  });
+  const setValue = (value: unknown) => {
+    try {
+      setStoredValue(value);
+      if (typeof window !== "undefined") {
+        window.localStorage.setItem(key, JSON.stringify(value));
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  return [storedValue, setValue];
+};
+
+export { useLocalStorage };
