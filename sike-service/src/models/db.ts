06b1e8bdfd809cbca59744f23df9cfbea4fcc140
Heinry-Anselm <50972784+anselm-chilz@users.noreply.github.com>
2024-11-26 12:27:18 +0000

    Further updates for vercel deployment

--- sike-service/src/models/db.ts
+++ sike-service/src/models/db.ts
@@ -1,18 +1,17 @@
-import mongoose from 'mongoose';
-import logger from '../utils/logger';
+import mongoose from "mongoose";
+import logger from "../utils/logger";
 
 export default async function connectDB(URL: string) {
   await mongoose
     .connect(URL, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useFindAndModify: false,
-      useCreateIndex: true,
+      writeConcern: {
+        w: "majority",
+      },
     })
     .then(() => {
-      logger.info('DB connected successfully');
+      logger.info("DB connected successfully");
     })
     .catch((e) => {
-      logger.info('DB connection failed: ', e);
+      logger.info("DB connection failed: ", e);
     });
 }
